d12ac55c7debe86d7059ec9232ccdb65d0b53ede
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-18 06:21:21 +0530

    Working on Chat list item

---
+++ src/components/ChatListItem/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const ChatListItem = () => {
+  return (
+    <View>
+      <Text>ChatListItem</Text>
+    </View>
+  );
+};
+
+export default ChatListItem;
