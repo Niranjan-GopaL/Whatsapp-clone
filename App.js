0cfff3ef0b32e1ffb8654731ea9dae2c8e1b3a7b
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-18 12:51:35 +0530

    CHatListItem styled and and set up almost completed

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ChatListItem from './src/components/ChatListItem';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello world </Text>
       <StatusBar style="auto" />
+      <ChatListItem />
     </View>
   );
 }
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,11 +1,56 @@
 import { View, Text } from "react-native";
+import dayjs from "dayjs";
 
 const ChatListItem = () => {
   return (
     <View>
-      <Text>ChatListItem</Text>
+      {/* <Text>ChatListItem what up</Text> */}
+      <Image source={{}} />
+
+      <View style={styles.content}>
+        <View style={styles.row}>
+          <Text style={styles.name} numberOfLines={1}>
+            UserName
+          </Text>
+          {/* <Text style={styles.subTitle}>{dayjs(chat.lastMessage.createdAt).fromNow(true)}</Text> */}
+        </View>
+        </View>
     </View>
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  content: {
+    flex: 1,
+
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'lightgray',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  name: {
+    flex: 1,
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    color: 'gray',
+  },
+});
+
 
 export default ChatListItem;
