f396de7150f4a93da8207aa9d779fa4108941c65
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-20 05:53:00 +0530

    Messages are dynamically rendered

--- src/Message/index.js
+++ src/Message/index.js
@@ -1,12 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);  // relative line is like "3 days ago, 4 hours  ago, 10 mins ago etc"
 
-const Message = () => {
+
+const Message = ({message}) => {
   return (
-    <View>
-      <Text>Message !!!! </Text>
+    <View style={styles.container}>
+      <Text style={styles.message}> 
+        {message.text}
+       </Text>
+
+      <Text style={styles.time}> 
+        {message.createdAt}
+       </Text>
     </View>
   );
 };
 
 export default Message;
+
+const styles = StyleSheet.create({
+    container: {
+      margin: 5,
+      padding: 10,
+      borderRadius: 10,
+      maxWidth: "80%",
+  
+    // Shadows
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.18,
+      shadowRadius: 1.0,
+  
+      elevation: 1,
+    },
+
+    message: {},
+
+    time: {
+      alignSelf: "flex-end",
+      color: "grey",
+    },
+  });
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground,FlatList } from "react-native";
+import { View, Text, ImageBackground,FlatList,StyleSheet } from "react-native";
 import bg from '../../assets/images/BG.png'
 import Message from "../Message";
 import messages from '../../assets/data/messages.json'
@@ -14,13 +14,29 @@ import messages from '../../assets/data/messages.json'
 
 const ChatScreen = () => {
   return (
-    <ImageBackground source={bg}>
+    <ImageBackground  style={styles.container} source={bg}>
+
         <FlatList 
-        
-        renderItem={{}}
+        data={messages}
+        renderItem={({ item }) => <Message message={item} />}
+        style={{ padding: 10 }}
+
+        // because we need to render the most recent first.
+        inverted
         />
+
     </ImageBackground>
   );
 };
 
 export default ChatScreen;
+
+const styles = StyleSheet.create({
+
+    container:{
+        flex:1
+    }
+
+
+
+})
