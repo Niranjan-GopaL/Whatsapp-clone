0b9f01bc933c98fb5436be0d5c4c89d12eabd208
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-23 01:29:08 +0530

    Navigator built using StackNavigator

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,11 +1,49 @@
-import { View, Text } from "react-native";
-import React from "react";
+import ChatScreen from "../screens/ChatScreen";
+import {NavigationContainer} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+
+
+/*
+
+---------------::   STACK NAVIGATOR  ::------------------------------
+
+        we use Stack navigator :-
+
+    - each screen is added to a stack when user triggers it
+    - and when the user goes back that screen is popped off
+    - there are some transitioning animation
+    - behind the scenes the actual Stack Data structure stores a 
+      lot of stuff including the path, and other metadata
+
+
+*/
+
+
+
+const Stack = createNativeStackNavigator();
+
+
+// We need to wrap all our screens with Navigator component
 const Navigator = () => {
   return (
-    <View>
-      <Text>Navigator</Text>
-    </View>
+
+    // we use Stack navigator :-
+    <NavigationContainer>
+
+      <Stack.Navigator 
+        initialRouteName="Chat" // this is used to set initial screen that needs to be displayed
+        screenOptions={{ headerStyle: { backgroundColor: 'whitesmoke' } 
+      }}>
+
+        {/* requires two props :- name and component                   */}
+        <Stack.Screen name="Home" component={MainTabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen name="Contacts" component={ContactsScreen} />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 };
 
